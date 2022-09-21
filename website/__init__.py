5d480c9d5f920d4c70e14951f024db7fa137a726
Kevin Stewart <94493923+kevin-stew@users.noreply.github.com>
2022-09-21 10:16:33 -0500

    first commit

---
+++ website/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_migrate import Migrate
+from config import Config
+
+from .site.routes import site
+from .authentication.routes import auth
+from .models import db as root_db, login_manager, ma
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+app.register_blueprint(site)
+app.register_blueprint(auth)
+
+root_db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = 'auth.login' #specify the page rendered for non-authed users
+ma.init_app(app)
+migrate = Migrate(app, root_db)
+
+
+if __name__ == "__main__":
+    app.run()
